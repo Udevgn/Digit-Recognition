9be0265f4c199b3c0dcfaef18a1812ca68a137cf
Udevgn <utkarshdevgan@yahoo.com>
2018-08-04 21:30:44 +0530

    data reading and split..
    reading the data and spliting it into the test and train data .

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("hello World")
-print("noobs here")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+
+train_df = pd.read_csv(r'data\')
+test_df  = pd.read_csv(r'data\')
+
+train_df.head()
+
+
+train_data = np.arrays(train_df,dtype='float32')
+test_data = np.array(test_df,dtype='float32')
+                       
+x_train = train_data[:,1:]
+y_train = train_data[:,0]
+
+x_test = train_data[:,1:]
+y_test = train_data[:,0]
+
+x_train,x_validate,y_test,y_validate = train_test_split(x_train,y_train,test_size=0.2,random_state=12345)
