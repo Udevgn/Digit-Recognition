823a9b91e1683e229d50559ef3f71d0fb592872a
Udevgn <utkarshdevgan@yahoo.com>
2018-08-05 21:06:42 +0530

    created nn with one hidden layer...
    epoch =20 ,optimizer = sgd, activation = relu

--- main.py
+++ main.py
@@ -6,27 +6,36 @@ from keras.models import Sequential
 from keras.layers import Conv2D,MaxPooling2D, Dense, Flatten, Dropout
 from keras.optimizers import Adam
 from keras.callbacks import TensorBoard
-#preparinf process...
-train_df = pd.read_csv(r'data\train.csv')
-test_df  = pd.read_csv(r'data\test.csv')
+from keras.optimizers import SGD
+import keras
+
+
+#preparing process...
+train_df = pd.read_csv(r'train.csv')
+test_df  = pd.read_csv(r'test.csv')
 
 train_df.head()
 
-train_data = np.arrays(train_df,dtype='float32')
-test_data = np.array(test_df,dtype='float32')
+feat_cols = train_df.columns.tolist()[1:]
+
+
                        
-x_train = train_data[:,1:]
-y_train = train_data[:,0]
+x_train =  train_df[feat_cols].values.reshape(-1,28,28,1)/255
+y_train =  train_df.iloc[:,0]
 
-x_test = train_data[:,1:]
-y_test = train_data[:,0]
+x_test =   test_df[feat_cols].values.reshape(-1,28,28,1)/255
+y_test =   test_df.iloc[:,0].values
 
-x_train,x_validate,y_test,y_validate = train_test_split(x_train,y_train,test_size=0.2,random_state=12345)
+x_train = x_train.reshape(-1, 784).astype('float32')
+x_test = x_test.reshape(-1, 784).astype('float32')
+
+y_train = keras.utils.to_categorical(y_train)
+y_test =  keras.utils.to_categorical(y_test)
+
+
+#x_train,x_validate,y_test,y_validate = train_test_split(x_train,y_train,test_size=0.2,random_state=12345)
 model = Sequential()
 model.add(Dense(64, activation='relu', input_dim=784))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
 #compilation process....
 
@@ -37,6 +46,6 @@ model.compile(loss='categorical_crossentropy',
 
 model.fit(x_train, y_train,
           epochs=20,
-          batch_size=128,validation_data=(x_validate,y_validate))
+          batch_size=128)
 score = model.evaluate(x_test, y_test, batch_size=128)
                        
